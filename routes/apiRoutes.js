d4e7b1cef1d02cb3a156b530d2dc9855b9c2923f
Esteban Meza <meza.19@osu.edu>
2021-03-20 03:02:51 -0400

    Update apiRoutes to fully get, post, and delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 // Load data
 let db = require("../db/db.json");
-
+const fs = require("fs");
 
 
 // Routing
@@ -9,17 +9,67 @@ module.exports = function(app) {
     // When user vists page...
     
     app.get("/api/notes", function(req, res) {
-        res.json(db);
+        // newDb = ()
+     
+        // res.json(db);
+        // console.log(db);
+        fs.readFile("db/db.json", "utf8" ,function(err, data) {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        } );
     });
 
-    app.post("/api/notes", function(req,res) {
-        db.push(req.body);
-        res.json(true);
+    app.post("/api/notes", function(req, res) {
+        const newNote = req.body;
+        fs.readFile("db/db.json", "utf8" ,function (err, data) {
+            
+            const parseData = JSON.parse(data);
+         
+            let id = 0;
+            if (parseData.length > 0 ) {
+                id = parseData[parseData.length -1].id + 1;
+            }
+            if (err) throw err;
+            newNote.id = id;
+            //db.push(newNote);
+             parseData.push(newNote);
+
+            fs.writeFile("db/db.json", JSON.stringify(parseData), function (err, data) {
+            if (err) throw err;
+            res.json(newNote);
+        });
+        })
+        
+
+        
+        // db.push(req.body);
+        // res.json(true);
     });
 
-    app.post("/api/clear", function(req, res) {
+    app.delete("/api/notes/:id", function(req, res) {
+        fs.readFile("db/db.json", "utf8" ,function(err, data) {
+            if (err) throw err;
+            let parseData = JSON.parse(data);
+            let noteToRemove = 0;
+            console.log("parse data is ", parseData);
+            console.log("req params id is ", req.params.id);
+            for (var i = 0; i < parseData.length; i++) {
+                if (parseData[i].id == req.params.id) {
+                        noteToRemove = i;
+                        
+                }
+            }
+            console.log("attempting to remove" , noteToRemove);
+              parseData.splice(noteToRemove, 1);
+             console.log("parse data is", parseData);
+            // for each ()
+            // res.json(JSON.parse(data));
+            fs.writeFile("db/db.json", JSON.stringify(parseData), function (err) {
+                if (err) throw err;
+                   res.json(parseData);
+            });
+        } );
         //Empty out arrays of note
-        db.length = 0;
-        res.json({ok:true});
+        // res.json({ok:true});
     });
 };
